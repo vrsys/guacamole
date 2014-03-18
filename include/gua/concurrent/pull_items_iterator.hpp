826ff5fb0f193ddf76d266688e26b70e43c1a24f
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2014-03-18 13:24:14 +0100

    Apply clang-format.

--- include/gua/concurrent/pull_items_iterator.hpp
+++ include/gua/concurrent/pull_items_iterator.hpp
@@ -19,8 +19,8 @@
  *                                                                            *
  ******************************************************************************/
 
-#ifndef GUA_PULL_ITEMS_ITERATOR_HPP
-#define GUA_PULL_ITEMS_ITERATOR_HPP
+#ifndef GUA_PULL_ITEMS_RANGE_HPP
+#define GUA_PULL_ITEMS_RANGE_HPP
 
 #include <boost/optional.hpp>
 #include <boost/iterator/iterator_facade.hpp>
@@ -29,82 +29,75 @@
 namespace gua {
 namespace concurrent {
 
-template< class T , class Container>
-class pull_items_range
-{
-    Container & sin_;
-    mutable T obj_;
+// Adapted from: 
+// http://ericniebler.com/2013/11/07/input-iterators-vs-input-ranges/
+template <class T, class Container> class pull_items_range {
+  Container& sin_;
+  mutable T obj_;
 
-    bool next() const
-    {
-      boost::optional<T> v = sin_->read();
-      if (v)
-        obj_ = *v;
-      return v;
+  bool next() const {
+    boost::optional<T> v = sin_->read();
+    if (v)
+      obj_ = *v;
+    return v;
+  }
+
+ public:
+
+  struct iterator
+      : boost::iterator_facade<iterator, T const, std::input_iterator_tag> {
+    iterator() : rng_ {}
+    {}
+
+   private:
+    friend class pull_items_range;
+    friend class boost::iterator_core_access;
+
+    explicit iterator(pull_items_range const& rng)
+        : rng_(rng ? &rng : nullptr) {}
+
+    void increment() {
+      // Don't advance a singular iterator
+      BOOST_ASSERT(rng_);
+      // Fetch the next element, null out the
+      // iterator if it fails
+      if (!rng_->next())
+        rng_ = nullptr;
     }
-public:
+    bool equal(iterator that) const { return rng_ == that.rng_; }
 
-    struct iterator
-      : boost::iterator_facade<
-            iterator,
-            T const,
-            std::input_iterator_tag
-        >
-    {
-        iterator() : rng_{} {}
-    private:
-        friend class pull_items_range;
-        friend class boost::iterator_core_access;
-
-        explicit iterator(pull_items_range const & rng)
-          : rng_(rng ? &rng : nullptr)
-        {}
-
-        void increment()
-        {
-            // Don't advance a singular iterator
-            BOOST_ASSERT(rng_);
-            // Fetch the next element, null out the
-            // iterator if it fails
-            if(!rng_->next())
-                rng_ = nullptr;
-        }
-        bool equal(iterator that) const
-        {
-            return rng_ == that.rng_;
-        }
-
-        T const & dereference() const
-        {
-            // Don't deref a singular iterator
-            BOOST_ASSERT(rng_);
-            return rng_->obj_;
-        }
-
-        pull_items_range const *rng_;
-    };
-    // Define const_iterator and iterator together:
-    typedef iterator const_iterator;
-
-    explicit pull_items_range(Container& sin)
-      : sin_(sin), obj_{}
-    {
-        next(); // prime the pump
+    T const& dereference() const {
+      // Don't deref a singular iterator
+      BOOST_ASSERT(rng_);
+      return rng_->obj_;
     }
 
-    iterator begin() const { return iterator{*this}; }
-    iterator end() const   { return iterator{};     }
+    pull_items_range const* rng_;
+  };
+  // Define const_iterator and iterator together:
+  typedef iterator const_iterator;
 
-    explicit operator bool() const // any objects left?
-    {
-        return !sin_->closed();
-    }
+  explicit pull_items_range(Container& sin) : sin_(sin), obj_ {} {
+    next();  // prime the pump
+  }
 
-    bool operator!() const { return !sin_; }
+  iterator begin() const {
+    return iterator { *this };
+  }
+
+  iterator end() const {
+    return iterator {};
+  }
+
+  explicit operator bool() const {
+    return !sin_->closed();
+  }
+
+  bool operator!() const { return !sin_; }
 };
 
 }
 
 }
 
-#endif  // GUA_PULL_ITEMS_ITERATOR_HPP
+#endif  // GUA_PULL_ITEMS_RANGE_HPP
