a1c1136304b4bcecab54427e176ecdefd487dab7
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2014-06-18 14:06:29 +0200

    Make last_context_id_ atomic.

--- include/gua/renderer/Window.hpp
+++ include/gua/renderer/Window.hpp
@@ -32,6 +32,7 @@
 #include <gua/utils/configuration_macro.hpp>
 
 // external headers
+#include <atomic>
 #include <memory>
 #include <string>
 #include <scm/gl_util/primitives/quad.h>
@@ -177,7 +178,7 @@ protected:
                bool clear = true);
 
 
-  static unsigned last_context_id_;
+  static std::atomic_uint last_context_id_;
 
   std::shared_ptr<WarpMatrix> warpRR_, warpGR_, warpBR_, warpRL_, warpGL_, warpBL_;
 };
