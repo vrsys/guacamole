e7f419198521cd54618050215ebefe352e6b5621
Andrey Babanin <andrey.babanin@uni-weimar.de>
2014-07-10 12:31:44 +0200

    Add window hints to GlfwWindow

--- src/gua/renderer/GlfwWindow.cpp
+++ src/gua/renderer/GlfwWindow.cpp
@@ -123,6 +123,11 @@ void GlfwWindow::open() {
     return;
   }
 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, config.get_debug());
+
   glfw_window_ = glfwCreateWindow(
     config.get_size().x, config.get_size().y,
     config.get_title().c_str(), nullptr, nullptr
