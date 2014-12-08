f5446478772e28985ccaa0d6c60b8e43c6fd4810
Sebastian Utzig <sebastian.utzig@uni-weimar.de>
2014-12-08 12:23:43 +0100

    added single material for pinkys normal map

--- examples/animation/main.cpp
+++ examples/animation/main.cpp
@@ -62,17 +62,17 @@ int main(int argc, char** argv) {
     return shader->get_default_material();
   };
 
-  auto mat1(load_mat("data/materials/SimpleMaterial.gmd"));
+  auto mat1(load_mat("data/materials/pinky.gmd"));
 
   gua::SkeletalAnimationLoader loader;
 
   auto transform = graph.add_node<gua::node::TransformNode>("/", "transform");
-  auto teapot(loader.create_geometry_from_file("bob", "data/objects/pinky.md5mesh", mat1, gua::SkeletalAnimationLoader::LOAD_MATERIALS | gua::SkeletalAnimationLoader::NORMALIZE_POSITION | gua::SkeletalAnimationLoader::NORMALIZE_SCALE));
+  auto teapot(loader.create_geometry_from_file("bob", "data/objects/pinky.md5mesh", mat1, gua::SkeletalAnimationLoader::NORMALIZE_POSITION | gua::SkeletalAnimationLoader::NORMALIZE_SCALE));
   
   std::shared_ptr<gua::node::SkeletalAnimationNode> node = std::dynamic_pointer_cast<gua::node::SkeletalAnimationNode, gua::node::Node>(teapot);
   node->get_director()->LoadAnimations(loader.load_animation("data/objects/idle1.md5anim", 0));
-  node->get_director()->LoadAnimations(loader.load_animation("data/objects/attack.md5anim", 0));
-  node->get_director()->LoadAnimations(loader.load_animation("data/objects/run.md5anim", 0));
+  //node->get_director()->LoadAnimations(loader.load_animation("data/objects/attack.md5anim", 0));
+  //node->get_director()->LoadAnimations(loader.load_animation("data/objects/run.md5anim", 0));
 
   graph.add_node("/transform", teapot);
   teapot->set_draw_bounding_box(true);
