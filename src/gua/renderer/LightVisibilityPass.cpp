d9e41b378d87172702e3f18e5c50e1b1f692f683
Andrey Babanin <andrey.babanin@uni-weimar.de>
2014-12-18 20:06:45 +0100

    Add other light types. Precompute some light properties on CPU

--- src/gua/renderer/LightVisibilityPass.cpp
+++ src/gua/renderer/LightVisibilityPass.cpp
@@ -22,16 +22,22 @@ void lighting(PipelinePass& pass, PipelinePassDescription const& , Pipeline& pip
   auto gl_program(ctx.render_context->current_program());
 
   // collect lights
-
+  std::vector<math::mat4> transforms;
   LightTable::array_type lights;
 
   // point lights
   for (auto const& l : pipe.get_scene().nodes[std::type_index(typeid(node::PointLightNode))]) {
     auto light(reinterpret_cast<node::PointLightNode*>(l));
 
+    auto model_mat = light->get_cached_world_transform();
+
+    math::vec3 light_position = model_mat * math::vec4(0.0, 0.0, 0.0, 1.0);
+    float light_radius = scm::math::length(light_position - math::vec3(model_mat * math::vec4(0.0, 0.0, 1.0, 1.0)));
+
     LightTable::LightBlock light_block {};
 
-    light_block.model_matrix    = light->get_cached_world_transform();
+    light_block.position_and_radius = math::vec4(light_position, light_radius);
+    light_block.beam_direction_and_half_angle = math::vec4(0.f, 0.f, 0.f, 0.f);
     light_block.color           = math::vec4(light->data.get_color().vec3(), 0.f);
     light_block.falloff         = light->data.get_falloff();
     light_block.brightness      = light->data.get_brightness();
@@ -42,11 +48,20 @@ void lighting(PipelinePass& pass, PipelinePassDescription const& , Pipeline& pip
     light_block.casts_shadow    = 0;
 
     lights.push_back(light_block);
+    transforms.push_back(model_mat);
   }
 
   // spot lights
   for (auto const& l : pipe.get_scene().nodes[std::type_index(typeid(node::SpotLightNode))]) {
     auto light(reinterpret_cast<node::SpotLightNode*>(l));
+
+    auto model_mat = light->get_cached_world_transform();
+
+    math::vec3 light_position = model_mat * math::vec4(0.0, 0.0, 0.0, 1.0);
+    float light_radius = scm::math::length(light_position - math::vec3(model_mat * math::vec4(0.0, 0.0, 1.0, 1.0)));
+    math::vec3 beam_direction = math::vec3(model_mat * math::vec4(0.0, 0.0, -1.0, 1.0)) - light_position;
+    float half_beam_angle = scm::math::dot(scm::math::normalize(math::vec3(model_mat * math::vec4(0.0, 0.5, -1.0, 0.0))),
+                                           scm::math::normalize(beam_direction));
 
     LightTable::LightBlock light_block {};
 
@@ -54,7 +69,8 @@ void lighting(PipelinePass& pass, PipelinePassDescription const& , Pipeline& pip
       // not implemented yet
     }
 
-    light_block.model_matrix    = light->get_cached_world_transform();
+    light_block.position_and_radius = math::vec4(light_position, light_radius);
+    light_block.beam_direction_and_half_angle = math::vec4(beam_direction, half_beam_angle);
     light_block.color           = math::vec4(light->data.get_color().vec3(), 0.f);
     light_block.falloff         = light->data.get_falloff();
     light_block.brightness      = light->data.get_brightness();
@@ -65,6 +81,7 @@ void lighting(PipelinePass& pass, PipelinePassDescription const& , Pipeline& pip
     light_block.casts_shadow    = 0; //light->data.get_enable_shadows();
 
     lights.push_back(light_block);
+    transforms.push_back(model_mat);
   }
 
   // proxy geometries
@@ -76,17 +93,16 @@ void lighting(PipelinePass& pass, PipelinePassDescription const& , Pipeline& pip
   pipe.get_light_table().invalidate(ctx, pipe.get_camera().config.get_resolution(), lights);
 
   // draw lights
-  int lid = 0;
-  for (const auto& light : lights) {
-    gl_program->uniform("gua_model_matrix", 0, light.model_matrix);
-    gl_program->uniform("light_id", 0, lid++);
+  for (size_t i = 0; i < lights.size(); ++i) {
+    gl_program->uniform("gua_model_matrix", 0, transforms[i]);
+    gl_program->uniform("light_id", 0, int(i));
     ctx.render_context->bind_image(pipe.get_light_table().get_light_bitset()->get_buffer(ctx), 
                                    scm::gl::FORMAT_R_32UI, scm::gl::ACCESS_READ_WRITE, 0, 0, 0);
     ctx.render_context->apply();
 
-    if (light.type == 0)
+    if (lights[i].type == 0)
       light_sphere->draw(ctx);
-    else if (light.type == 1)
+    else if (lights[i].type == 1)
       light_cone->draw(ctx);
   }
 
