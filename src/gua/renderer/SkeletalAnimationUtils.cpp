2f668d9ee43dccc3ed6b667fcae183842cdee714
Jakob Wagner <wobakjerna@web.de>
2015-01-06 18:11:46 +0100

    node is also class

--- src/gua/renderer/SkeletalAnimationUtils.cpp
+++ src/gua/renderer/SkeletalAnimationUtils.cpp
@@ -6,14 +6,6 @@
 
 namespace gua {
 
-void SkeletalAnimationUtils::collect_bone_indices(std::map<std::string, int>& ids, std::shared_ptr<Node> const& pNode) {
-  ids[pNode->name] = pNode->index;
-
-  for(std::shared_ptr<Node>& child : pNode->children) {
-    collect_bone_indices(ids, child);
-  }
-}
-
 std::shared_ptr<Node> SkeletalAnimationUtils::load_hierarchy(aiScene const* scene) {
   //construct hierarchy
   std::shared_ptr<Node> root = std::make_shared<Node>(scene->mRootNode);
@@ -31,19 +23,8 @@ std::shared_ptr<Node> SkeletalAnimationUtils::load_hierarchy(aiScene const* scen
       }
     }
   }
- set_bone_properties(bone_info, root);
+  root->set_properties(bone_info);
   return root;
-}
-
-void SkeletalAnimationUtils::set_bone_properties(std::map<std::string, std::pair<uint, scm::math::mat4f>> const& infos, std::shared_ptr<Node>& pNode) {
-  if(infos.find(pNode->name) != infos.end()) {
-    pNode->offsetMatrix = infos.at(pNode->name).second;
-    pNode->index = infos.at(pNode->name).first;
-  }
-
-  for(std::shared_ptr<Node>& child : pNode->children) {
-    set_bone_properties(infos, child);
-  }
 }
 
 std::vector<std::shared_ptr<SkeletalAnimation>> SkeletalAnimationUtils::load_animations(aiScene const* scene) {
@@ -70,39 +51,9 @@ void SkeletalAnimationUtils::calculate_matrices(float timeInSeconds, std::shared
   }
 
   scm::math::mat4f identity = scm::math::mat4f::identity();
-  accumulate_matrices(transforms, root, pose, identity);
+  root->accumulate_matrices(transforms, pose, identity);
 }
 
-void SkeletalAnimationUtils::accumulate_matrices(std::vector<scm::math::mat4f>& transformMat4s, std::shared_ptr<Node> const& pNode, Pose const& pose, scm::math::mat4f const& parentTransform) {
-  scm::math::mat4f nodeTransformation{pNode->transformation};
-
-  if(pose.contains(pNode->name)) { 
-    nodeTransformation = pose.get_transform(pNode->name).to_matrix();  
-  }
-  
-  scm::math::mat4f finalTransformation = parentTransform * nodeTransformation;
-
-  //update transform if bone is mapped
-  if (pNode->index >= 0) {
-    transformMat4s[pNode->index] = finalTransformation * pNode->offsetMatrix;
-  }
-  
-  for (uint i = 0 ; i < pNode->numChildren ; i++) {
-    accumulate_matrices(transformMat4s, pNode->children[i], pose, finalTransformation);
-  }
-}
-
-std::shared_ptr<Node> SkeletalAnimationUtils::find_node(std::string const& name, std::shared_ptr<Node> const& root) {
-  if(root->name == name) return root;
-
-  for(std::shared_ptr<Node> const& child : root->children) {
-
-    std::shared_ptr<Node> found = find_node(name, child);
-    if (found) return found;
-  }
-
-  return nullptr;
-}
 ////////////////////////////////////////////////////////////////////////////////
 float Blend::cos(float x) {
   x *= scm::math::pi_f;
@@ -130,6 +81,74 @@ float Blend::swap(float x)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Node::Node(aiNode const* node):
+  index{-1},
+  name{node->mName.C_Str()},
+  parentName{node->mParent != NULL ? node->mParent->mName.C_Str() : "none"},
+  numChildren{node->mNumChildren},
+  transformation{ai_to_gua(node->mTransformation)},
+  offsetMatrix{scm::math::mat4f::identity()}
+{
+  for(unsigned i = 0; i < node->mNumChildren; ++i) {
+    std::shared_ptr<Node> child = std::make_shared<Node>(node->mChildren[i]);
+    children.push_back(child);
+  }
+}
+
+Node::~Node()
+{}
+
+std::shared_ptr<Node> Node::find(std::string const& name) const {
+
+  for(std::shared_ptr<Node> const& child : children) {
+    if(child->name == name) return child;
+
+    std::shared_ptr<Node> found = child->find(name);
+    if(found) return found;
+  }
+
+  return nullptr;
+}
+
+void Node::collect_indices(std::map<std::string, int>& ids) const {
+  ids[name] = index;
+
+  for(std::shared_ptr<Node> const& child : children) {
+    child->collect_indices(ids);
+  }
+}
+
+void Node::set_properties(std::map<std::string, std::pair<uint, scm::math::mat4f>> const& infos) {
+  if(infos.find(name) != infos.end()) {
+    offsetMatrix = infos.at(name).second;
+    index = infos.at(name).first;
+  }
+
+  for(std::shared_ptr<Node>& child : children) {
+    child->set_properties(infos);
+  }
+}
+
+void Node::accumulate_matrices(std::vector<scm::math::mat4f>& transformMat4s, Pose const& pose, scm::math::mat4f const& parentTransform) const {
+  scm::math::mat4f nodeTransformation{transformation};
+
+  if(pose.contains(name)) { 
+    nodeTransformation = pose.get_transform(name).to_matrix();  
+  }
+  
+  scm::math::mat4f finalTransformation = parentTransform * nodeTransformation;
+
+  //update transform if bone is mapped
+  if (index >= 0) {
+    transformMat4s[index] = finalTransformation * offsetMatrix;
+  }
+  
+  for (std::shared_ptr<Node> const& child : children) {
+     child->accumulate_matrices(transformMat4s, pose, finalTransformation);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 Transformation::Transformation():
   scaling{1.0f},
   rotation{scm::math::quatf::identity()},
@@ -143,7 +162,7 @@ Transformation::Transformation(scm::math::vec3 const& scale, scm::math::quatf co
 {}
 
 Transformation::~Transformation()
-{};
+{}
 
 scm::math::mat4f Transformation::to_matrix() const {
   return scm::math::make_translation(translation) * rotation.to_matrix() * scm::math::make_scale(scaling);
@@ -178,7 +197,7 @@ BoneAnimation::BoneAnimation():
 {}
 
 BoneAnimation::~BoneAnimation()
-{};
+{}
 
 BoneAnimation::BoneAnimation(aiNodeAnim* anim):
   name{anim->mNodeName.C_Str()}
@@ -276,7 +295,7 @@ SkeletalAnimation::SkeletalAnimation(aiAnimation* anim):
 }
 
 SkeletalAnimation::~SkeletalAnimation()
-{};
+{}
 
 Pose SkeletalAnimation::calculate_pose(float time) const { 
   Pose pose{};
@@ -302,7 +321,7 @@ Pose::Pose():
 {}
 
 Pose::~Pose()
-{};
+{}
 
 bool Pose::contains(std::string const& name ) const {
   return transforms.find(name) != transforms.end();
