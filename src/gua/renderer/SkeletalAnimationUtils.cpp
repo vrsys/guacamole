3d34e315f2ee1343e2f93d4b43f049a29fe28291
Jakob Wagner <wobakjerna@web.de>
2015-01-15 17:41:17 +0100

    loading normals works

--- src/gua/renderer/SkeletalAnimationUtils.cpp
+++ src/gua/renderer/SkeletalAnimationUtils.cpp
@@ -394,6 +394,7 @@ void Mesh::copy_to_buffer(Vertex* vertex_buffer)  const {
     vertex_buffer[v].bone_ids = scm::math::vec4i(weights[v].IDs[0],weights[v].IDs[1],weights[v].IDs[2],weights[v].IDs[3]);
   }
 }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 Transformation::Transformation():
   scaling{1.0f},
