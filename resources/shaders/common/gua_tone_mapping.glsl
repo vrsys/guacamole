f89c6a829aece7116d997f21c20e4b19fa5d13e1
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2015-03-04 13:50:13 +0100

    Add new tonemapping method.

--- resources/shaders/common/gua_tone_mapping.glsl
+++ resources/shaders/common/gua_tone_mapping.glsl
@@ -25,6 +25,29 @@ vec3 toneMapReinhard(vec3 linearColor, float exposure)
   return pow(linearColor, vec3(1.0/2.2));
 }
 
+// http://www.slideshare.net/ozlael/hable-john-uncharted2-hdr-lighting
+// #define A 0.22f // shoulder strength
+// #define B 0.30f // linear strength
+// #define C 0.10 // Linear angle
+// #define D 0.20 // Toe Strength
+// #define E 0.01 // Toe Numerator
+// E/F is Toe Angle
+// F(x) = ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F;
+// FinalColor = F(LinearColor)/F(LinearWhite)
+
+const float A = 0.15; // ShoulderStrength
+const float B = 0.50; // LinearStrength
+const float C = 0.10; // LinearAngle
+const float D = 0.20; // ToeStrength
+const float E = 0.02; // ToeNumerator
+const float F = 0.30; // ToeDenominator
+const float W = 11.2; // LinearWhite
+
+vec3 Uncharted2Tonemap(vec3 linearColor)
+{
+  return ((linearColor*(A*linearColor+C*B)+D*E)/(linearColor*(A*linearColor+B)+D*F))-E/F;
+}
+
 vec3 toneMap(vec3 col)
 {
   switch (@gua_tone_mapping_method@) {
@@ -34,6 +57,11 @@ vec3 toneMap(vec3 col)
       return toneMapHejl(col, gua_tone_mapping_exposure);
     case 2:
       return toneMapReinhard(col, gua_tone_mapping_exposure);
+    case 3:
+      vec3 curr = Uncharted2Tonemap(gua_tone_mapping_exposure*col);
+      vec3 whiteScale = 1.0f/Uncharted2Tonemap(vec3(W));
+      vec3 color = curr*whiteScale;
+      return pow(color, vec3(1.0/2.2));
   }
   return toneMapLinear(col, gua_tone_mapping_exposure);
 }
--- src/gua/renderer/ResolvePass.cpp
+++ src/gua/renderer/ResolvePass.cpp
@@ -49,12 +49,13 @@ ResolvePassDescription::ResolvePassDescription()
   uniforms["gua_background_mode"] = static_cast<int>(BackgroundMode::COLOR);
   uniforms["gua_background_color"] = scm::math::vec3f(0.2f, 0.2f, 0.2f);
   uniforms["gua_background_texture"] = std::string("gua_default_texture");
-  
+
   // default ambient lighting
   uniforms["gua_environment_lighting_mode"] = static_cast<int>(EnvironmentLightingMode::AMBIENT_COLOR);
-  uniforms["gua_environment_lighting_color"] = scm::math::vec3f(0.2f, 0.2f, 0.2f);
+  uniforms["gua_environment_lighting_color"] = scm::math::vec3f(0.05f, 0.05f, 0.05f);
   uniforms["gua_environment_lighting_spheremap"] = std::string("gua_default_texture");
   uniforms["gua_environment_lighting_cubemap"] = std::string("gua_default_texture");
+  uniforms["gua_horizon_fade"] = 1.3f;
 
   // default ssao
   uniforms["gua_ssao_enable"] = false;
@@ -62,7 +63,7 @@ ResolvePassDescription::ResolvePassDescription()
   uniforms["gua_ssao_intensity"] = 1.0f;
   uniforms["gua_ssao_falloff"] = 0.1f;
   uniforms["gua_noise_tex"] = std::string("gua_default_texture");
-  
+
   // default fog configuration
   uniforms["gua_enable_fog"] = false;
   uniforms["gua_fog_start"] = 10.f;
@@ -155,6 +156,19 @@ ResolvePassDescription::EnvironmentLightingMode ResolvePassDescription::environm
   auto uniform(uniforms.find("gua_environment_lighting_mode"));
   return static_cast<ResolvePassDescription::EnvironmentLightingMode>(boost::get<int>(uniform->second.data));
 }
+
+////////////////////////////////////////////////////////////////////////////////
+ResolvePassDescription& ResolvePassDescription::horizon_fade(float radius) {
+  uniforms["gua_horizon_fade"] = radius;
+  return *this;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+float ResolvePassDescription::horizon_fade() const {
+  auto uniform(uniforms.find("gua_horizon_fade"));
+  return boost::get<float>(uniform->second.data);
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 ResolvePassDescription& ResolvePassDescription::ssao_enable(bool enable) {
