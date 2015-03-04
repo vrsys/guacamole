f0161f7050d7603830710a711074cb80abef0489
Jakob Wagner <wobakjerna@web.de>
2015-03-04 13:10:34 +0100

    made skeletal animation plugin

---
+++ plugins/guacamole-skelanim/include/gua/node/SkeletalAnimationNode.hpp
@@ -0,0 +1,147 @@
+/******************************************************************************
+ * guacamole - delicious VR                                                   *
+ *                                                                            *
+ * Copyright: (c) 2011-2013 Bauhaus-Universität Weimar                        *
+ * Contact:   felix.lauer@uni-weimar.de / simon.schneegans@uni-weimar.de      *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU General Public License as published by the Free *
+ * Software Foundation, either version 3 of the License, or (at your option)  *
+ * any later version.                                                         *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License   *
+ * for more details.                                                          *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program. If not, see <http://www.gnu.org/licenses/>.             *
+ *                                                                            *
+ ******************************************************************************/
+
+#ifndef GUA_SKELETAL_ANIMATION_NODE_HPP
+#define GUA_SKELETAL_ANIMATION_NODE_HPP
+
+// guacamole headers
+#include <gua/node/GeometryNode.hpp>
+#include <gua/renderer/SkeletalAnimationDirector.hpp>
+
+namespace gua {
+
+class SkeletalAnimationRessource;
+class SkeletalAnimationLoader;
+
+namespace node {
+
+/**
+ * This class is used to represent polygonal geometry in the SceneGraph.
+ *
+ * \ingroup gua_scenegraph
+ */
+class GUA_DLL SkeletalAnimationNode : public GeometryNode {
+
+public : // typedef/enums/friends
+
+  friend class ::gua::SkeletalAnimationLoader;
+
+  SkeletalAnimationNode(std::string const& node_name = "",
+              std::vector<std::string> const& geometry_description = {},
+              std::vector<std::shared_ptr<Material>> const& materials = {},
+              std::shared_ptr<SkeletalAnimationDirector> animation_director = nullptr,
+              math::mat4 const& transform = math::mat4::identity());
+
+private : // c'tor / d'tor
+
+public : // methods
+
+
+  Node* get();
+
+  /**
+  * Get the strings referring to an entry in guacamole's GeometryDatabase.
+  */
+  std::vector<std::string> const& get_geometry_descriptions() const;
+
+  /**
+  * Set the string referring to an entry in guacamole's GeometryDatabase.
+  */
+  void set_geometry_description(std::string const& geometry_description, uint index);
+
+  /**
+  * A string referring to an entry in guacamole's MaterialShaderDatabase.
+  */
+
+  std::vector<std::shared_ptr<Material>>&       get_materials();
+
+  std::shared_ptr<Material>                     get_material(uint index) const;
+
+  void            set_material(std::shared_ptr<Material> material, uint index);
+
+  /**
+  * Implements ray picking for a triangular mesh
+  */
+  void ray_test_impl(Ray const& ray,
+                     int options,
+                     Mask const& mask,
+                     std::set<PickResult>& hits) override;
+
+  /**
+  * Updates bounding box by accessing the ressource in the databse
+  */
+  void update_bounding_box() const override;
+
+  void update_bone_transforms(RenderContext const& ctx);
+
+  void update_cache() override;
+
+  std::vector<std::shared_ptr<SkeletalAnimationRessource>> const& get_geometries() const;
+  
+  std::shared_ptr<SkeletalAnimationDirector> const& get_director() const;
+
+  void set_animation_mode(uint mode);
+  uint get_animation_mode();
+  
+  void set_blending_mode(uint mode);
+  uint get_blending_mode();
+
+  std::string get_animation()const;
+  void        set_animation(std::string);
+
+  float get_blending_factor()const;
+  void set_blending_factor(float f);
+
+  std::vector<math::BoundingBox<math::vec3>> get_bone_boxes();
+
+  /**
+   * Accepts a visitor and calls concrete visit method.
+   *
+   * This method implements the visitor pattern for Nodes.
+   *
+   * \param visitor  A visitor to process the GeometryNode's data.
+   */
+  void accept(NodeVisitor& visitor) override;
+
+ protected:
+
+  std::shared_ptr<Node> copy() const override;
+
+ private:  // attributes e.g. special attributes for drawing
+
+  std::vector<std::shared_ptr<SkeletalAnimationRessource>> geometries_;
+  std::vector<std::string>          geometry_descriptions_;
+  bool                              geometry_changed_;
+
+  std::vector<std::shared_ptr<Material>>             materials_;
+
+  bool                              material_changed_;
+
+  std::shared_ptr<SkeletalAnimationDirector> animation_director_;
+  std::shared_ptr<BoneTransformUniformBlock> bone_transforms_block_;
+
+  bool first_run_;
+};
+
+} // namespace node {
+} // namespace gua {
+
+#endif  // GUA_SKELETAL_ANIMATION_NODE_HPP
---
+++ plugins/guacamole-skelanim/src/gua/renderer/SkeletalAnimationDirector.cpp
@@ -0,0 +1,234 @@
+// class header
+#include <gua/renderer/SkeletalAnimationDirector.hpp>
+
+
+// guacamole headers
+#include <gua/utils/Logger.hpp>
+
+//external headers
+#include <iostream>
+
+namespace gua 
+{
+SkeletalAnimationDirector::SkeletalAnimationDirector(std::shared_ptr<Node> const& root):
+    num_bones_{0},
+    has_anims_{false},
+    firstRun_{true},
+    animations_{},
+    currAnimation_{nullptr},
+    animNum_{0},
+    animNumLast_{0},
+    bone_mapping_{},
+    anim_start_node_{},
+    state_{Playback::crossfade},
+    blending_state_{Blending::linear},
+    blendFactor_{0.0},
+    //next_transition_{0},
+    next_blending_end_{0},
+    root_{root},
+    timer_{} {
+  timer_.start();
+
+  root_->collect_indices(bone_mapping_);
+  num_bones_ = bone_mapping_.size();
+  //prevent doom models from moving during anim
+  if(root_->name == "<MD5_Root>") {
+    anim_start_node_ = root_->children[1]->children[0]->children[0];
+  }
+  else {
+    anim_start_node_ = root_;
+  }
+}
+
+void SkeletalAnimationDirector::add_animations(aiScene const& scene) {
+  std::vector<std::shared_ptr<SkeletalAnimation>> newAnims{SkeletalAnimationUtils::load_animations(scene)};
+  if(newAnims.empty()) return;
+
+  animations_.reserve(animations_.size() + newAnims.size());
+  std::move(newAnims.begin(), newAnims.end(), std::inserter(animations_, animations_.end()));
+  newAnims.clear();
+
+  has_anims_ = animations_.size() > 0;
+  currAnimation_ = animations_[animations_.size()-1];
+}
+void SkeletalAnimationDirector::add_animations(FbxScene& scene) {
+  std::vector<std::shared_ptr<SkeletalAnimation>> newAnims{SkeletalAnimationUtils::load_animations(scene)};
+  if(newAnims.empty()) return;
+
+  animations_.reserve(animations_.size() + newAnims.size());
+  std::move(newAnims.begin(), newAnims.end(), std::inserter(animations_, animations_.end()));
+  newAnims.clear();
+
+  has_anims_ = animations_.size() > 0;
+  currAnimation_ = animations_[animations_.size()-1];
+}
+
+void SkeletalAnimationDirector::blend_pose(float timeInSeconds, SkeletalAnimation const& pAnim1, SkeletalAnimation const& pAnim2, std::vector<scm::math::mat4f>& transforms) {
+
+  float timeNormalized1 = timeInSeconds / pAnim1.get_duration();
+  timeNormalized1 = scm::math::fract(timeNormalized1);
+
+  float timeNormalized2 = timeInSeconds / pAnim2.get_duration();
+  timeNormalized2 = scm::math::fract(timeNormalized2);
+
+  scm::math::mat4f identity = scm::math::mat4f::identity();
+
+  Pose pose1{pAnim1.calculate_pose(timeNormalized1)};
+  Pose pose2{pAnim2.calculate_pose(timeNormalized2)};
+  
+  pose1.blend(pose2, blendFactor_);
+
+  anim_start_node_->accumulate_matrices(transforms, pose1, identity);
+}
+
+void SkeletalAnimationDirector::partial_blend(float timeInSeconds, SkeletalAnimation const& pAnim1, SkeletalAnimation const& pAnim2, std::string const& nodeName, std::vector<scm::math::mat4f>& transforms) {
+  
+  std::shared_ptr<Node> start{anim_start_node_->find(nodeName)};
+  
+  if(!start) {
+    Logger::LOG_WARNING << "node '"<< nodeName << "' not found" << std::endl; 
+    return;
+  }
+
+  float timeNormalized1 = timeInSeconds / pAnim1.get_duration();
+  timeNormalized1 = scm::math::fract(timeNormalized1);
+
+  float timeNormalized2 = timeInSeconds / pAnim2.get_duration();
+  timeNormalized2 = scm::math::fract(timeNormalized2);
+
+  scm::math::mat4f identity = scm::math::mat4f::identity();
+
+  Pose full_body{pAnim1.calculate_pose(timeNormalized1)};
+  Pose upper_body{pAnim2.calculate_pose(timeNormalized2)};
+
+  full_body.partial_replace(upper_body, start);
+
+  anim_start_node_->accumulate_matrices(transforms, full_body, identity);
+}
+
+std::vector<scm::math::mat4f> SkeletalAnimationDirector::get_bone_transforms()
+{
+  //reserve vector for transforms
+  std::vector<scm::math::mat4f> transforms{num_bones_, scm::math::mat4f::identity()};
+  
+  float currentTime = timer_.get_elapsed();
+
+  if(!has_anims_) {
+    SkeletalAnimationUtils::calculate_matrices(*anim_start_node_, transforms);
+    return transforms;  
+  }
+
+  switch(state_) {
+    //crossfade two anims
+    case Playback::crossfade: {
+      //float blendDuration = 2;
+      //float playDuration = animations_[animNum_]->get_duration();
+
+      //else if(currentTime <= next_transition_ + blendDuration) {
+      //if(currentTime <= next_transition_ + blendDuration) {
+      if(currentTime <= next_blending_end_) {
+        float time = 1.f-((next_blending_end_ - currentTime) / blendDuration_);
+        
+        blendFactor_ = 0.0;
+        switch(blending_state_){
+          case Blending::swap : blendFactor_ = blend::swap(time);break;
+          case Blending::linear : blendFactor_ = blend::linear(time);break;
+          case Blending::smoothstep : blendFactor_ = blend::smoothstep(time);break;
+          case Blending::cosinus : blendFactor_ = blend::cos(time);break;
+          default: blendFactor_ = blend::linear(time);
+        }
+        
+        blend_pose(currentTime, *animations_[animNumLast_], *animations_[animNum_], transforms);
+      }
+      //if(currentTime < next_transition_) {
+      else {
+        if (blendFactor_ != 1.0){
+          blendFactor_ = 1.0;
+        }
+        SkeletalAnimationUtils::calculate_matrices(currentTime, *anim_start_node_, *animations_[animNum_], transforms);  
+      }
+      /*else {
+        next_transition_ = currentTime + playDuration;
+        animNum_ = (animNum_ + 1) % animations_.size();
+      }*///loop through all given animations
+      break;
+    }
+    //blend two anims
+    case Playback::partial: {
+      partial_blend(currentTime, *animations_[0], *animations_[1], "Waist", transforms);
+      break;
+    }
+    default: Logger::LOG_WARNING << "playback mode not found" << std::endl; break;
+  }
+  
+  return transforms;
+}
+
+void SkeletalAnimationDirector::set_playback_mode(uint mode) {
+  switch(mode) {
+    case 0 : state_ = Playback::partial; break;
+    case 1 : state_ = Playback::crossfade; break;
+    default: state_ = Playback::partial;
+  }
+}
+
+uint SkeletalAnimationDirector::get_playback_mode() {
+  return state_;
+}
+
+void SkeletalAnimationDirector::set_blending_mode(uint mode) {
+  switch(mode) {
+    case 0 : blending_state_ = Blending::swap; break;
+    case 1 : blending_state_ = Blending::linear; break;
+    case 2 : blending_state_ = Blending::smoothstep; break;
+    case 3 : blending_state_ = Blending::cosinus; break;
+    default: blending_state_ = Blending::linear;;
+  }
+}
+
+uint SkeletalAnimationDirector::get_blending_mode() {
+  return blending_state_;
+}
+
+int SkeletalAnimationDirector::getBoneID(std::string const& name) {
+  return bone_mapping_.at(name);
+}
+
+
+std::string const& SkeletalAnimationDirector::get_animation() const {
+  if(currAnimation_!=nullptr){
+    return currAnimation_->get_name();
+  }
+  else{
+    return "";
+  }
+}
+
+void SkeletalAnimationDirector::set_animation(std::string animation_name) {
+  for(uint i{0};i<animations_.size();++i){
+    if(animations_[i]->get_name()==animation_name){
+      animNumLast_ = animNum_;
+      animNum_ = i;
+      currAnimation_ = animations_[animNum_];
+      float currentTime = timer_.get_elapsed();
+      //next_transition_ = currentTime + animations_[animNum_]->get_duration();
+      next_blending_end_ = currentTime + blendDuration_;
+      return;
+    }
+  }
+  gua::Logger::LOG_WARNING << "No matching animation with name: "<< animation_name<<" found!" << std::endl;
+}
+
+float SkeletalAnimationDirector::get_blending_factor()const{
+  return blendFactor_;
+}
+
+void SkeletalAnimationDirector::set_blending_factor(float f){
+  blendFactor_ = f;
+}
+
+bool SkeletalAnimationDirector::has_anims() const {
+  return has_anims_;
+}
+
+} // namespace gua
