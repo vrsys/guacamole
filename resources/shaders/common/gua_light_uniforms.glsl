82c2a4d21a1486760995d8f3d4de9d8c497310c3
Simon Schneegans <code@simonschneegans.de>
2015-03-31 13:36:24 +0200

    moved light uniform block to include

---
+++ resources/shaders/common/gua_light_uniforms.glsl
@@ -0,0 +1,23 @@
+uniform int gua_lights_num;
+uniform int gua_sun_lights_num;
+
+struct LightSource {
+  vec4   position_and_radius; // xyz - position (or direction for sun light), w - radius
+  vec4   beam_direction_and_half_angle; //  xyz - direction, w - half angle
+  vec4   color;
+  float  falloff;
+  float  brightness;
+  float  softness;
+  uint   type;           // 0 - point, 1 - spot, 2 - sun
+  bool   diffuse_enable;
+  bool   specular_enable;
+  bool   casts_shadow;
+  //uint   pad;
+
+  float  shadow_offset;
+  uvec2  shadow_map;
+};
+
+layout(std140, binding=1) uniform lightBlock {
+  LightSource gua_lights[@max_lights_num@];
+};
--- resources/shaders/common/gua_shading.glsl
+++ resources/shaders/common/gua_shading.glsl
@@ -1,32 +1,10 @@
-// lights
-uniform uvec2 gua_light_bitset;
-uniform int gua_lights_num;
-uniform int gua_sun_lights_num;
-
-struct LightSource {
-  vec4   position_and_radius; // xyz - position (or direction for sun light), w - radius
-  vec4   beam_direction_and_half_angle; //  xyz - direction, w - half angle
-  vec4   color;
-  float  falloff;
-  float  brightness;
-  float  softness;
-  uint   type;           // 0 - point, 1 - spot, 2 - sun
-  bool   diffuse_enable;
-  bool   specular_enable;
-  bool   casts_shadow;
-  //uint   pad;
-
-  float  shadow_offset;
-  uvec2  shadow_map;
-};
-
-layout(std140, binding=1) uniform lightBlock {
-  LightSource gua_lights[@max_lights_num@];
-};
-
 const float Pi = 3.14159265;
 const float INV_PI = 1.0f / Pi;
 
+// lights
+uniform uvec2 gua_light_bitset;
+
+@include "gua_light_uniforms.glsl"
 @include "../brdf.glsl"
 
 // -----------------------------------------------------------------------------
