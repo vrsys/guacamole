43464a0c17093da4641a8a3352464997628364d0
Jakob Harlan <jakob.harlan@uni-weimar.de>
2015-06-30 16:10:56 +0200

    Remove stupidity

--- src/gua/renderer/Pipeline.cpp
+++ src/gua/renderer/Pipeline.cpp
@@ -448,9 +448,7 @@ std::shared_ptr<Texture2D> Pipeline::render_scene(
     if (!depth_cube_map_) {
       depth_cube_map_ = std::make_shared<DepthCubeMap>(context_, math::vec2ui(map_width, viewport_size));;
     } else {
-      if (context_.framecount % 120 == 0) {
-        depth_cube_map_->retrieve_data(context_, current_viewstate_.camera.config.near_clip(), current_viewstate_.camera.config.far_clip());
-      }
+      depth_cube_map_->retrieve_data(context_, current_viewstate_.camera.config.near_clip(), current_viewstate_.camera.config.far_clip());
     }
 
 
