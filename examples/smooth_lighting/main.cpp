8e039e3541219d4bd62c7fb2bd936e5cd1f19882
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2015-11-25 16:54:46 +0100

    Explicitly load textures

--- examples/smooth_lighting/main.cpp
+++ examples/smooth_lighting/main.cpp
@@ -215,9 +215,10 @@ int main(int argc, char** argv) {
   rough_white->set_uniform("emissivity", 0.0f);
 
   scm::gl::sampler_state_desc const& sampler_state_desc = scm::gl::sampler_state_desc(scm::gl::FILTER_ANISOTROPIC, scm::gl::WRAP_MIRRORED_REPEAT, scm::gl::WRAP_MIRRORED_REPEAT);
-  gua::TextureDatabase::instance()->add("data/textures/envlightmap.jpg", std::make_shared<gua::Texture2D>("data/textures/envlightmap.jpg", true, sampler_state_desc));
-
-
+  //gua::TextureDatabase::instance()->add("data/textures/envlightmap.jpg", std::make_shared<gua::Texture2D>("data/textures/envlightmap.jpg", true, sampler_state_desc));
+  gua::TextureDatabase::instance()->load("data/textures/envlightmap.jpg");
+  gua::TextureDatabase::instance()->load("data/textures/envmap.jpg");
+  gua::TextureDatabase::instance()->load("data/textures/noise.jpg");
   rough_white->set_uniform("noise_texture", std::string("data/textures/noise.jpg"));
 
 
