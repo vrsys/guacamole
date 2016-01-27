8b5aa9a7418e7e6929bcfa5e791c42dd1ef3f695
Andre Schollmeyer <mail@ansm.de>
2016-01-27 18:01:08 +0100

    adapted externals find scripts

--- cmake/modules/find_ovr.cmake
+++ cmake/modules/find_ovr.cmake
@@ -2,7 +2,7 @@
 # search paths
 ##############################################################################
 SET(OVR_INCLUDE_SEARCH_DIRS
-  ${GLOBAL_EXT_DIR}/inc/ovr/include
+  ${GLOBAL_EXT_DIR}/LibOVR/include
   ${OVR_INCLUDE_DIRS}
   ${OVR_INCLUDE_SEARCH_DIR}
   /opt/OculusSDK/currentSDK2/LibOVR/Include
@@ -10,7 +10,7 @@ SET(OVR_INCLUDE_SEARCH_DIRS
 )
 
 SET(OVR_LIBRARY_SEARCH_DIRS
-  ${GLOBAL_EXT_DIR}/lib
+  ${GLOBAL_EXT_DIR}/LibOVR/lib
   ${OVR_LIBRARY_DIRS}
   ${OVR_LIBRARY_SEARCH_DIR}
   /opt/OculusSDK/currentSDK2/LibOVR/Lib/Linux/x86_64/Release
@@ -24,8 +24,7 @@ message(STATUS "-- checking for Oculus SDK")
 find_path(OVR_INCLUDE_DIR NAMES OVR.h Kernel/OVR_Types.h PATHS ${OVR_INCLUDE_SEARCH_DIRS})
 
 IF (MSVC)
-	find_library(OVR_LIBRARY NAMES libOVR64.lib PATHS ${OVR_LIBRARY_SEARCH_DIRS} PATH_SUFFIXES release)
-	find_library(OVR_LIBRARY_DEBUG NAMES libOVR64.lib PATHS ${OVR_LIBRARY_SEARCH_DIRS} PATH_SUFFIXES debug)
+	find_library(OVR_LIBRARY NAMES libOVR64.lib libOVR.lib PATHS ${OVR_LIBRARY_SEARCH_DIRS} PATH_SUFFIXES release)
 ELSEIF (UNIX)
 	find_library(OVR_LIBRARY NAMES libOVRRT64_0.so PATHS ${OVR_LIBRARY_SEARCH_DIRS})
 ENDIF (MSVC)
