521d7e0ec69d31ad2e6a017e8846cfa89ef37e3b
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2016-03-14 16:12:59 +0100

    Use RenderContext::Mesh in SkinnedMeshResource

--- plugins/guacamole-skelanim/include/gua/renderer/SkinnedMeshResource.hpp
+++ plugins/guacamole-skelanim/include/gua/renderer/SkinnedMeshResource.hpp
@@ -108,15 +108,12 @@ class GUA_SKELANIM_DLL SkinnedMeshResource : public GeometryResource {
   friend class LightingPass;
 
  private:
-
+  void init_bone_boxes();
   void upload_to(RenderContext& ctx) /*const*/;
 
   SkinnedMesh mesh_;
 
-  mutable std::vector<scm::gl::buffer_ptr> vertices_;
-  mutable std::vector<scm::gl::buffer_ptr> indices_;
-  mutable std::vector<scm::gl::vertex_array_ptr> vertex_array_;
-  mutable std::mutex upload_mutex_;
+  mutable std::mutex upload_mutex_; // neeed for bone_boxes_
 
   std::vector<math::BoundingBox<math::vec3> > bone_boxes_;
 
