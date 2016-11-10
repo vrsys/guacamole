7af81113eb7b56e53a957ce4fda9413894e58169
Jakob Wagner <wobakjerna@web.de>
2016-11-10 14:44:26 +0100

    define GUA_SKELANIM_DLL macro in skelanim/platform.hpp

--- plugins/guacamole-skelanim/include/gua/skelanim/utils/SkeletalAnimationPass.hpp
+++ plugins/guacamole-skelanim/include/gua/skelanim/utils/SkeletalAnimationPass.hpp
@@ -23,7 +23,7 @@
 #define GUA_SLELETAL_ANIMATION_PASS_HPP
 
 // guacamole headers
-#include <gua/Skelanim.hpp>
+#include <gua/skelanim/platform.hpp>
 #include <gua/renderer/PipelinePass.hpp>
 
 // external headers
