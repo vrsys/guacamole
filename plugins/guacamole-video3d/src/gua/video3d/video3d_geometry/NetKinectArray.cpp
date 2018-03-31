aa267f035c252c2163c305ae989929c8bb3c847f
Lucas Huebner <vaza0835@greyarea.medien.uni-weimar.de>
2018-03-31 13:17:55 +0200

    added feedback loop

--- plugins/guacamole-video3d/src/gua/video3d/video3d_geometry/NetKinectArray.cpp
+++ plugins/guacamole-video3d/src/gua/video3d/video3d_geometry/NetKinectArray.cpp
@@ -60,24 +60,30 @@ void NetKinectArray::readloop() {
 
   // open multicast listening connection to server and port
   zmq::context_t ctx(1); // means single threaded
-  zmq::socket_t  socket(ctx, ZMQ_SUB); // means a subscriber
+  zmq::socket_t  socket(ctx, ZMQ_SUB);    // means a subscriber
+  zmq::socket_t  socket_f(ctx, ZMQ_PUB);  // means a Publisher
 
   socket.setsockopt(ZMQ_SUBSCRIBE, "", 0);
 #if ZMQ_VERSION_MAJOR < 3
   int64_t hwm = 1;
   socket.setsockopt(ZMQ_HWM, &hwm, sizeof(hwm));
+  socket_f.setsockopt(ZMQ_HWM, &hwm, sizeof(hwm));
 #else
   int hwm = 1;
   socket.setsockopt(ZMQ_RCVHWM, &hwm, sizeof(hwm));
+  socket_f.setsockopt(ZMQ_RCVHWM, &hwm, sizeof(hwm));
 #endif
   std::string endpoint("tcp://" + m_server_endpoint);
+  std::string endpoint_f("tcp://141.54.147.108:7001");
   socket.connect(endpoint.c_str());
+  socket_f.bind(endpoint_f.c_str());
 
 
 
   //const unsigned message_size = (m_colorsize_byte + m_depthsize_byte) * m_calib_files.size();
   //std::cout << m_calib_files.size() << "; " <<  m_colorsize_byte << " : " << size.colorsize_byte << "; " <<  m_depthsize_byte << " : " << size.depthsize_byte <<std::endl;
   
+  unsigned cont = 0;
   while (m_running) {
 
     //zmq::message_t zmqm(message_size);
@@ -89,6 +95,25 @@ void NetKinectArray::readloop() {
     while (m_need_swap) {
       ;
     }
+
+    zmq::message_t zmqm_f(6*sizeof(float));
+    std::vector<float> test_inputs;
+    test_inputs.push_back(cont);
+    cont += 1;
+    test_inputs.push_back(cont);
+    cont += 1;
+    test_inputs.push_back(cont);
+    cont += 1;
+    test_inputs.push_back(cont);
+    cont += 1;
+    test_inputs.push_back(cont);
+    cont += 1;
+    test_inputs.push_back(cont);
+    cont += 1;
+    
+    memcpy(zmqm_f.data(), test_inputs.data(), 6*sizeof(float));    
+    socket_f.send(zmqm_f);
+
 
     const unsigned message_size = (size.colorsize_byte + size.depthsize_byte) * num_streams; 
     if(m_buffer_back.size() < message_size) {
