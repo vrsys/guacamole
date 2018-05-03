7d2350f54079c55db943db35b2d929ebe6e06224
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2018-05-03 15:53:29 +0200

    fix factor for automatic point size computation

--- plugins/guacamole-spoints/src/gua/spoints/SPointsRenderer.cpp
+++ plugins/guacamole-spoints/src/gua/spoints/SPointsRenderer.cpp
@@ -327,12 +327,13 @@ void SPointsRenderer::render(Pipeline& pipe,
       unsigned const remote_renderer_screen_width = spoints_resource->get_remote_server_screen_width();
       unsigned const remote_renderer_screen_height = spoints_resource->get_remote_server_screen_height();
 
-      float target_ratio = 1.20;
 
-      float scale_x = (target_ratio * render_target_dims.x) / remote_renderer_screen_width;
-      float scale_y = (target_ratio * render_target_dims.y) / remote_renderer_screen_height;
+
+      float scale_x = (render_target_dims.x) / remote_renderer_screen_width;
+      float scale_y = (render_target_dims.y) / remote_renderer_screen_height;
       //render_target_dims.x 
 
+      float target_ratio = 1.20;
       float screen_space_point_size = std::max(target_ratio, std::max(scale_x, scale_y) );
 
       current_shader->set_uniform(
