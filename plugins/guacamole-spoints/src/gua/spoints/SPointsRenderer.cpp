294714e4bb523af1d9bb5dc71b63fd69c642f840
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2018-05-03 15:28:35 +0200

    fix bug in height estimation for automatic spoints size calculation

--- plugins/guacamole-spoints/src/gua/spoints/SPointsRenderer.cpp
+++ plugins/guacamole-spoints/src/gua/spoints/SPointsRenderer.cpp
@@ -327,10 +327,10 @@ void SPointsRenderer::render(Pipeline& pipe,
       unsigned const remote_renderer_screen_width = spoints_resource->get_remote_server_screen_width();
       unsigned const remote_renderer_screen_height = spoints_resource->get_remote_server_screen_height();
 
-      float target_ratio = 1.33;
+      float target_ratio = 1.20;
 
-      float scale_x = target_ratio * render_target_dims.x / remote_renderer_screen_width;
-      float scale_y = target_ratio * render_target_dims.x / remote_renderer_screen_width;
+      float scale_x = (target_ratio * render_target_dims.x) / remote_renderer_screen_width;
+      float scale_y = (target_ratio * render_target_dims.y) / remote_renderer_screen_height;
       //render_target_dims.x 
 
       float screen_space_point_size = std::max(target_ratio, std::max(scale_x, scale_y) );
