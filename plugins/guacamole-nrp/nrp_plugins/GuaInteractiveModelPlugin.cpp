85bd2437025d4d81d15d3f0fc0b7298040784fb1
Anton <anton.frolov@uni-weimar.de>
2018-08-01 00:41:53 +0200

    * write pos at world update;

--- plugins/guacamole-nrp/nrp_plugins/GuaInteractiveModelPlugin.cpp
+++ plugins/guacamole-nrp/nrp_plugins/GuaInteractiveModelPlugin.cpp
@@ -14,8 +14,14 @@ void gazebo::GuaInteractiveModelPlugin::Load(gazebo::physics::ModelPtr parent, s
     _sub = _node->Subscribe("/nrp-gua/interactive_pos", &GuaInteractiveModelPlugin::callback_pos, this);
 
     _model = parent;
+
+    this->_update_connection = event::Events::ConnectWorldUpdateBegin(boost::bind(&GuaInteractiveModelPlugin::on_update, this));
 }
 void gazebo::GuaInteractiveModelPlugin::callback_pos(ConstPosesStampedPtr &msg)
 {
-    _model->SetWorldPose(msgs::ConvertIgn(msg->pose(0)), true, true);
+    _pose = msgs::ConvertIgn(msg->pose(0));
+}
+void gazebo::GuaInteractiveModelPlugin::on_update()
+{
+    _model->SetWorldPose(_pose, true, true);
 }
