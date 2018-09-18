f2d9b0e0b9aad705a699e4f78cc54f15ee293bd2
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2018-09-18 09:21:58 +0200

    remove a few compiler warnings and add extend cmake configuration for vt plugin

---
+++ cmake/modules/FindLamureVT.cmake
@@ -0,0 +1,74 @@
+##############################################################################
+# search paths
+##############################################################################
+SET(LAMURE_VT_ROOT
+  ../lamure/virtual_texturing/
+)
+
+
+SET(LAMURE_VT_INCLUDE_SEARCH_DIRS
+  ${LAMURE_VT_ROOT}/include
+  ${GLOBAL_EXT_DIR}/lamure/include
+  ${LAMURE_VT_INCLUDE_SEARCH_DIR}
+  /usr/include
+  /opt/local
+  #../lamure/include
+)
+
+SET(LAMURE_VT_LIBRARY_SEARCH_DIRS
+  ${GLOBAL_EXT_DIR}/lamure/lib
+  ${LAMURE_VT_LIBRARY_SEARCH_DIR}
+  ${LAMURE_VT_ROOT}/install/lib
+  /usr/lib
+  /usr/lib/x86_64-linux-gnu
+  /opt/local/lib
+  ../lamure/install/lib
+)
+
+message(error " LAMURE_VT_LIBRARY_SEARCH_DIRS ${LAMURE_VT_LIBRARY_SEARCH_DIRS}")
+
+##############################################################################
+# search
+##############################################################################
+message(STATUS "-- checking for LAMURE")
+
+find_path(LAMURE_VT_COMMON_INCLUDE_DIR NAMES /lamure/vt/common.h PATHS ${LAMURE_VT_INCLUDE_SEARCH_DIRS})
+find_path(LAMURE_VT_RENDERING_INCLUDE_DIR NAMES /lamure/vt/ren/CutUpdate.h PATHS ${LAMURE_VT_INCLUDE_SEARCH_DIRS})
+find_path(LAMURE_VT_PREPROCESSING_INCLUDE_DIR NAMES /lamure/vt/pre/Preprocessor.h PATHS ${LAMURE_VT_INCLUDE_SEARCH_DIRS})
+
+#find_path(LAMURE_TEST NAMES /lamure/vt/pre/Preprocessor.h PATHS ${LAMURE_VT_INCLUDE_SEARCH_DIRS})
+
+IF (MSVC)
+	#find_library(LAMURE_COMMON_LIBRARY NAMES lamure_common.lib PATHS ${LAMURE_LIBRARY_SEARCH_DIRS} PATH_SUFFIXES release)
+  #find_library(LAMURE_PREPROCESSING_LIBRARY NAMES lamure_preprocessing.lib PATHS ${LAMURE_LIBRARY_SEARCH_DIRS} PATH_SUFFIXES release)
+  #find_library(LAMURE_RENDERING_LIBRARY NAMES lamure_rendering.lib PATHS ${LAMURE_LIBRARY_SEARCH_DIRS} PATH_SUFFIXES release)
+  
+  #find_library(LAMURE_COMMON_LIBRARY_DEBUG NAMES lamure_commond.lib PATHS ${LAMURE_LIBRARY_SEARCH_DIRS} PATH_SUFFIXES debug)
+  #find_library(LAMURE_PREPROCESSING_LIBRARY_DEBUG NAMES lamure_preprocessingd.lib PATHS ${LAMURE_LIBRARY_SEARCH_DIRS} PATH_SUFFIXES debug)
+  #find_library(LAMURE_RENDERING_LIBRARY_DEBUG NAMES lamure_renderingd.lib PATHS ${LAMURE_LIBRARY_SEARCH_DIRS} PATH_SUFFIXES debug)
+
+ELSEIF (UNIX)
+
+  find_library(LAMURE_VT_LIBRARY NAMES liblamure_virtual_texturing.so PATHS ${LAMURE_VT_LIBRARY_SEARCH_DIRS})
+	#find_library(LAMURE_COMMON_LIBRARY NAMES liblamure_common.so PATHS ${LAMURE_LIBRARY_SEARCH_DIRS})
+  #find_library(LAMURE_PREPROCESSING_LIBRARY NAMES liblamure_preprocessing.so PATHS ${LAMURE_LIBRARY_SEARCH_DIRS})
+  #find_library(LAMURE_RENDERING_LIBRARY NAMES liblamure_rendering.so PATHS ${LAMURE_LIBRARY_SEARCH_DIRS})
+
+  #set(LAMURE_COMMON_LIBRARY_DEBUG ${LAMURE_COMMON_LIBRARY})
+  #set(LAMURE_PREPROCESSING_LIBRARY_DEBUG ${LAMURE_PREPROCESSING_LIBRARY})
+  #set(LAMURE_RENDERING_LIBRARY_DEBUG ${LAMURE_RENDERING_LIBRARY})
+
+ENDIF (MSVC)
+
+##############################################################################
+# verification
+##############################################################################
+include(FindPackageHandleStandardArgs)
+#find_package_handle_standard_args(LAMURE DEFAULT_MSG
+#    LAMURE_COMMON_LIBRARY LAMURE_RENDERING_LIBRARY
+#    LAMURE_COMMON_INCLUDE_DIR LAMURE_RENDERING_INCLUDE_DIR)
+find_package_handle_standard_args(LAMURE DEFAULT_MSG
+    LAMURE_VT_LIBRARY LAMURE_VT_RENDERING_INCLUDE_DIR)
+if(LAMURE_FOUND)
+   set(LAMURE_VT_INCLUDE_DIRS ${LAMURE_VT_COMMON_INCLUDE_DIR} ${LAMURE_VT_RENDERING_INCLUDE_DIR})
+endif()
---
+++ include/gua/virtual_texturing/VirtualTexture2D.hpp
@@ -0,0 +1,80 @@
+/******************************************************************************
+ * guacamole - delicious VR                                                   *
+ *                                                                            *
+ * Copyright: (c) 2011-2013 Bauhaus-Universität Weimar                        *
+ * Contact:   felix.lauer@uni-weimar.de / simon.schneegans@uni-weimar.de      *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU General Public License as published by the Free *
+ * Software Foundation, either version 3 of the License, or (at your option)  *
+ * any later version.                                                         *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License   *
+ * for more details.                                                          *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program. If not, see <http://www.gnu.org/licenses/>.             *
+ *                                                                            *
+ ******************************************************************************/
+
+#ifndef GUA_VIRTUALTEXTURE2D_HPP
+#define GUA_VIRTUALTEXTURE2D_HPP
+
+// guacamole headers
+#include <gua/platform.hpp>
+#include <gua/renderer/RenderContext.hpp>
+#include <gua/renderer/Texture.hpp>
+#include <gua/math/math.hpp>
+#include <gua/utils/Logger.hpp>
+
+
+//#include <lamure/vt/VTConfig.h>
+
+#include <scm/gl_util/data/imaging/texture_image_data.h>
+
+// external headers
+#include <string>
+#include <vector>
+
+#include <mutex>
+#include <thread>
+#include <memory>
+
+namespace gua {
+
+class GUA_DLL VirtualTexture2D : public Texture {
+ public:
+  
+  VirtualTexture2D(std::string const& file,
+                   scm::gl::sampler_state_desc const& state_descripton =
+                   scm::gl::sampler_state_desc(scm::gl::FILTER_ANISOTROPIC,
+                                               scm::gl::WRAP_REPEAT,
+                                               scm::gl::WRAP_REPEAT));
+
+  unsigned width() const override { return width_; }
+  unsigned height() const override { return height_; }
+
+  void upload_to(RenderContext const& context) const override;
+  void initialize_index_texture(RenderContext const& ctx, uint64_t cut_id) const;
+
+
+
+ protected:
+  scm::gl::texture_image_data_ptr image_ = nullptr;
+  unsigned width_;
+  unsigned height_;
+  unsigned layers_;
+
+ private:
+  std::string _file_config;
+  std::string _file_atlas;  
+  scm::shared_ptr<scm::gl::render_device> _device;
+  mutable scm::math::vec2ui                                                         _index_texture_dimension;
+  scm::gl::sampler_state_ptr                                                        _filter_nearest;
+  scm::gl::sampler_state_ptr                                                        _filter_linear;
+};  
+
+}
+#endif  // GUA_VIRTUALTEXTURE2D_HPP
