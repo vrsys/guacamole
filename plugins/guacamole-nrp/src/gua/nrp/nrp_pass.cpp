ff44372cdda3a3a11e1f36f12a11e4e39292d2dd
Anton <anton.frolov@uni-weimar.de>
2019-01-22 15:09:25 +0100

    * nrp + video3d,lod plugins simultaneous execution;

--- plugins/guacamole-nrp/src/gua/nrp/nrp_pass.cpp
+++ plugins/guacamole-nrp/src/gua/nrp/nrp_pass.cpp
@@ -4,13 +4,19 @@ namespace gua
 {
 namespace nrp
 {
-NRPPassDescription::NRPPassDescription() {}
+NRPPassDescription::NRPPassDescription() : PipelinePassDescription()
+{
+    needs_color_buffer_as_input_ = false;
+    writes_only_color_buffer_ = false;
+    enable_for_shadows_ = true;
+    rendermode_ = RenderMode::Custom;
+}
 std::shared_ptr<PipelinePassDescription> NRPPassDescription::make_copy() const { return std::make_shared<NRPPassDescription>(*this); }
 PipelinePass NRPPassDescription::make_pass(const RenderContext &ctx, SubstitutionMap &substitution_map)
 {
     PipelinePass pass{*this, ctx, substitution_map};
 
-    pass.process_ = [](PipelinePass &pass, PipelinePassDescription const &desc, Pipeline &pipe) {NRPBinder::get_instance().pre_render(); };
+    pass.process_ = [](PipelinePass &pass, PipelinePassDescription const &desc, Pipeline &pipe) { NRPBinder::get_instance().pre_render(); };
 
     return pass;
 }
