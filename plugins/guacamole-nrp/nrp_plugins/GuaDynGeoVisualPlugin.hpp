17db0355c66f2989211e6cf9b3001161041ae419
Anton <anton.frolov@uni-weimar.de>
2019-01-25 16:53:23 +0100

    *

--- plugins/guacamole-nrp/nrp_plugins/GuaDynGeoVisualPlugin.hpp
+++ plugins/guacamole-nrp/nrp_plugins/GuaDynGeoVisualPlugin.hpp
@@ -6,13 +6,13 @@
 
 #include <mutex>
 
+#include <Ogre.h>
 #include <gazebo/common/Color.hh>
 #include <gazebo/common/Events.hh>
 #include <gazebo/common/Time.hh>
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/rendering/Visual.hh>
 #include <gazebo/transport/Node.hh>
-#include <Ogre.h>
 
 typedef const boost::shared_ptr<gazebo::msgs::PosesStamped const> ConstPosesStampedPtr;
 
@@ -30,9 +30,10 @@ class GAZEBO_VISIBLE GuaDynGeoVisualPlugin : public VisualPlugin
     virtual void Load(rendering::VisualPtr _visual, sdf::ElementPtr _sdf);
 
   private:
-
     rendering::VisualPtr _visual;
     event::ConnectionPtr _update_connection;
+    Ogre::SceneNode *_scene_node;
+    Ogre::SceneManager *_scene_manager;
     void Update();
 };
 }
