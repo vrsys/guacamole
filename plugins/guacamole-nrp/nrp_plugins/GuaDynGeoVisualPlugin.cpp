f8754f8861db52300374e324d5f91959cccf8a9c
Anton <anton.frolov@uni-weimar.de>
2019-02-05 14:27:07 +0100

    *

--- plugins/guacamole-nrp/nrp_plugins/GuaDynGeoVisualPlugin.cpp
+++ plugins/guacamole-nrp/nrp_plugins/GuaDynGeoVisualPlugin.cpp
@@ -576,16 +576,16 @@ void GuaDynGeoVisualPlugin::Update()
     std::cerr << std::endl << "DynGeo: pre-render update before" << std::endl;
 #endif*/
 
-#if GUA_DEBUG == 1
-    std::thread::id this_id = std::this_thread::get_id();
-
-    gzerr << std::endl << "DynGeo: Update thread " << this_id << std::endl;
-    std::cerr << std::endl << "DynGeo: Update thread " << this_id << std::endl;
-#endif
-
     if(_is_initialized.load() && _is_need_swap.load())
     {
         std::unique_lock<std::mutex> lk_swap(_mutex_swap);
+
+#if GUA_DEBUG == 1
+        std::thread::id this_id = std::this_thread::get_id();
+
+        gzerr << std::endl << "DynGeo: Update thread " << this_id << std::endl;
+        std::cerr << std::endl << "DynGeo: Update thread " << this_id << std::endl;
+#endif
 
 #if GUA_DEBUG == 1
         gzerr << std::endl << "DynGeo: swap" << std::endl;
