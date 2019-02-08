173b16604ac180dd2e13d6ffea1f0cd2d1c92148
Anton <anton.frolov@uni-weimar.de>
2019-02-08 15:48:49 +0100

    *

--- plugins/guacamole-nrp/nrp_plugins/GuaDynGeoVisualPlugin.cpp
+++ plugins/guacamole-nrp/nrp_plugins/GuaDynGeoVisualPlugin.cpp
@@ -692,7 +692,7 @@ void GuaDynGeoVisualPlugin::Update()
         std::cerr << std::endl << "DynGeo: swap" << std::endl;
 #endif
 
-        // UpdateTriangleSoup();
+        UpdateTriangleSoup();
         _is_need_swap.store(false);
         _cv_recv_swap.notify_one();
     }
