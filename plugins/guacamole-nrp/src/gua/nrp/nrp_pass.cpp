12ae1e289b1cbe3ac16779312c691ef1ad9b6902
Anton <anton.frolov@uni-weimar.de>
2019-05-08 14:30:34 +0200

    * skelanim, nrp plugin compatibility;

--- plugins/guacamole-nrp/src/gua/nrp/nrp_pass.cpp
+++ plugins/guacamole-nrp/src/gua/nrp/nrp_pass.cpp
@@ -6,18 +6,17 @@ namespace nrp
 {
 NRPPassDescription::NRPPassDescription() : PipelinePassDescription()
 {
-    needs_color_buffer_as_input_ = false;
-    writes_only_color_buffer_ = false;
-    enable_for_shadows_ = true;
-    rendermode_ = RenderMode::Custom;
+    private_.needs_color_buffer_as_input_ = false;
+    private_.writes_only_color_buffer_ = false;
+    private_.enable_for_shadows_ = true;
+    private_.rendermode_ = RenderMode::Custom;
 }
 std::shared_ptr<PipelinePassDescription> NRPPassDescription::make_copy() const { return std::make_shared<NRPPassDescription>(*this); }
 PipelinePass NRPPassDescription::make_pass(const RenderContext &ctx, SubstitutionMap &substitution_map)
 {
+    private_.process_ = [](PipelinePass &pass, PipelinePassDescription const &desc, Pipeline &pipe) { NRPBinder::get_instance().pre_render(); };
+
     PipelinePass pass{*this, ctx, substitution_map};
-
-    pass.process_ = [](PipelinePass &pass, PipelinePassDescription const &desc, Pipeline &pipe) { NRPBinder::get_instance().pre_render(); };
-
     return pass;
 }
 } // namespace nrp
