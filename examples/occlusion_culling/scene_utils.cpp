d2f809d9d371af2bba45db50fc8532b66ba01a73
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2019-11-11 15:51:35 +0100

    change dimensions of random box

--- examples/occlusion_culling/scene_utils.cpp
+++ examples/occlusion_culling/scene_utils.cpp
@@ -1,6 +1,8 @@
 
 #include "scene_utils.hpp"
 
+
+
 void place_objects_randomly(std::string const& model_path, std::shared_ptr<gua::node::TransformNode>& scene_root_node) {
     for(int i = 0; i < 1000; ++i) {
         std::string const random_object_name = "obj_" + std::to_string(i);
@@ -10,16 +12,13 @@ void place_objects_randomly(std::string const& model_path, std::shared_ptr<gua::
 
         auto new_model(loader.create_geometry_from_file(random_object_name, model_path, model_mat, gua::TriMeshLoader::NORMALIZE_SCALE | gua::TriMeshLoader::MAKE_PICKABLE));
         
-        float rand_x_trans = 7.0 * std::rand() / (float)RAND_MAX - 3.5;
-        float rand_y_trans = 7.0 * std::rand() / (float)RAND_MAX - 3.5;
-        float rand_z_trans = 7.0 * std::rand() / (float)RAND_MAX - 3.5;
+        float rand_x_trans = 8.0 * std::rand() / (float)RAND_MAX - 4.0;
+        float rand_y_trans = 8.0 * std::rand() / (float)RAND_MAX - 4.0;
+        float rand_z_trans = 8.0 * std::rand() / (float)RAND_MAX - 4.0;
 
         float rand_angle  = 360.0 * std::rand() / (float)RAND_MAX;
         float rand_angle_2 = 360.0 * std::rand() / (float)RAND_MAX;
         float rand_angle_3 = 360.0 * std::rand() / (float)RAND_MAX;
-        float rand_x_rot = 360.0 * std::rand() / (float)RAND_MAX;
-        float rand_y_rot = 360.0 * std::rand() / (float)RAND_MAX;
-        float rand_z_rot = 360.0 * std::rand() / (float)RAND_MAX;
 
         //new_model->rotate(i, 0.0, 1.0, 0.0);
         //new_model->translate(rand_x_trans, rand_y_trans, rand_z_trans);
@@ -39,4 +38,11 @@ void place_objects_randomly(std::string const& model_path, std::shared_ptr<gua::
         //new_model->set_draw_bounding_box(true);
         scene_root_node->add_child(new_model);
     }
+}
+
+
+
+void rebuild_object_hierarchy_SAH_based(std::shared_ptr<gua::node::TransformNode>& scene_root_node, 
+                                        std::vector<std::shared_ptr<gua::node::TransformNode>>& geometry_nodes) {
+
 }
