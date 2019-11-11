a6fae1063bc0bfd357d7eedcb130bceee10c5a5c
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2019-11-11 15:51:35 +0100

    change dimensions of random box

--- examples/occlusion_culling/scene_utils.hpp
+++ examples/occlusion_culling/scene_utils.hpp
@@ -5,4 +5,6 @@
 
 void place_objects_randomly(std::string const& model_path, std::shared_ptr<gua::node::TransformNode>& scene_root_node);
 
+void rebuild_object_hierarchy_SAH_based(std::shared_ptr<gua::node::TransformNode>& scene_root_node);
+
 #endif
