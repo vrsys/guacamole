6a8ae1ba7680789c786a4c3d3ee596dd8d5ba8eb
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2019-11-12 10:27:10 +0100

    comment occlusion culling app further

--- examples/occlusion_culling/main.cpp
+++ examples/occlusion_culling/main.cpp
@@ -28,13 +28,18 @@
 #include <gua/guacamole.hpp>
 #include <gua/renderer/TriMeshLoader.hpp>
 #include <gua/renderer/ToneMappingPass.hpp>
+#include <gua/renderer/BBoxPass.hpp> //to add a pass that visualizes bounding boxes in gua
 #include <gua/renderer/DebugViewPass.hpp>
 #include <gua/utils/Logger.hpp>
 #include <gua/utils/Trackball.hpp>
 
 
-// global state variables
+// global variables
 extern WASD_state cam_navigation_state;  //only declared in main - definition is in navigation.cpp
+
+std::string model_path = "data/objects/teapot.obj"; //place this object
+int32_t num_models_to_place = 1000; //place 1000 objects
+float one_d_cube_size = 8.0; //8m*8m*8m cube for random object placement
 
 
 void adjust_arguments(int& argc, char**& argv)
@@ -47,9 +52,8 @@ void adjust_arguments(int& argc, char**& argv)
 
 
 // helper for command line parsing
-std::string parse_model_from_cmd_line(int argc, char** argv)
+void parse_model_from_cmd_line(int argc, char** argv)
 {
-    std::string model_path = "data/objects/teapot.obj";
 
     std::string log_message_model_string = "";
     if(argc < 2)
@@ -61,16 +65,24 @@ std::string parse_model_from_cmd_line(int argc, char** argv)
     {
         model_path = argv[1];
         log_message_model_string = std::string(argv[0]) + ": Using provided model path:";
+
+        if(argc > 2) {
+            num_models_to_place = std::atoi(argv[2]);
+
+            if(argc > 3) {
+                one_d_cube_size = std::atof(argv[3]);
+            }
+        }
     }
     gua::Logger::LOG_MESSAGE << log_message_model_string + model_path << std::endl;
 
-    return model_path;
+    
 }
 
 int main(int argc, char** argv)
 {
     // contains the model's obj path - path to the teapot if no argument is supplied, otherwise it is the path that the user provides
-    std::string const model_path = parse_model_from_cmd_line(argc, argv);
+    parse_model_from_cmd_line(argc, argv);
 
     // guacamole is rewriting the arguments, so we need this additiona call
     adjust_arguments(argc, argv);
@@ -109,12 +121,12 @@ int main(int argc, char** argv)
 
 
     // add a cluster of pseudorandomly placed objects in the scene. See: scene_utils.cpp 
-    place_objects_randomly(model_path, scene);
+    place_objects_randomly(model_path, num_models_to_place, scene);
 
     // add a point light source to the scene and attach it to the tranform node
     auto light = graph.add_node<gua::node::LightNode>("/transform", "light");
     light->data.set_type(gua::node::LightNode::Type::POINT);
-    light->data.brightness = 150.0f;
+    light->data.brightness = 350.0f;
     light->scale(12.f);
     light->translate(-3.f, 5.f, 5.f);
 
@@ -135,25 +147,48 @@ int main(int argc, char** argv)
     gua::utils::Trackball trackball(0.01, 0.002, 0.2);
 
     // setup rendering pipeline and window
-    auto resolution = gua::math::vec2ui(1920, 1080);
+    auto resolution = gua::math::vec2ui(2560, 1440);
 
 
-    auto resolve_pass = std::make_shared<gua::ResolvePassDescription>();
-    resolve_pass->background_mode(gua::ResolvePassDescription::BackgroundMode::QUAD_TEXTURE);
-    resolve_pass->tone_mapping_exposure(1.0f);
+    /* guacamole supports different rendering primitives - Triangle Meshes, LOD-PointClouds, Volumes, RGBD Streams, etc.
+       It is our responsibility to keep the rendering stages minimal by describing the geometry that we actually plan to render
+       
+       In addition to the Geoemtry passes, we usually want to have shading in the scene. 
+       For this, we add the LightVisibilityPass and ResolvePassDescription after the geometry passes.
+       After the resolve pass, we may add post processing passes (or a DebugView)
 
+    */
+    auto pipeline_description = std::make_shared<gua::PipelineDescription>();     
+    pipeline_description->add_pass(std::make_shared<gua::TriMeshPassDescription>());         // geometry pass for rendering trimesh files (obj, ply, ...)
+    pipeline_description->add_pass(std::make_shared<gua::BBoxPassDescription>());            // geometry pass for rendering bounding boxes of nodes
+    //----------------------------------------------------------------------------------------
+    pipeline_description->add_pass(std::make_shared<gua::LightVisibilityPassDescription>()); // treats the light as geometry and rasterizes it into a light buffer
+    pipeline_description->add_pass(std::make_shared<gua::ResolvePassDescription>());         // resolves the shading in screen space
+    pipeline_description->add_pass(std::make_shared<gua::DebugViewPassDescription>());       // visualizes the GBuffer-content
+
+    // configure the resolve pass
+    pipeline_description->get_resolve_pass()->tone_mapping_exposure(3.f);
+    pipeline_description->get_resolve_pass()->tone_mapping_method(gua::ResolvePassDescription::ToneMappingMethod::UNCHARTED);
+
+
+    //add a camera node. Without a camera, we can not render
     auto camera = graph.add_node<gua::node::CameraNode>("/navigation_node", "cam");
+    //we just leave the camera in 0, 0, 0 in its local coordinates
     camera->translate(0, 0, 0);
     camera->config.set_resolution(resolution);
+    //we tell the camera to which screen it belongs (camera position and screen boundaries define a frustum)
     camera->config.set_screen_path("/navigation_node/screen");
+    //we associate the camera with our scenegraph
     camera->config.set_scene_graph_name("main_scenegraph");
+    //and finally tell it in which window to render
     camera->config.set_output_window_name("main_window");
+    //for now, we do not render in stereo
     camera->config.set_enable_stereo(false);
+    //we associate our current pipeline description with the camera
+    camera->set_pipeline_description(pipeline_description);
 
 
-    camera->get_pipeline_description()->get_resolve_pass()->tone_mapping_exposure(1.0f);
-    camera->get_pipeline_description()->add_pass(std::make_shared<gua::DebugViewPassDescription>());
-
+    //create the window and configure it with correct resolution, stereo mode and window events
     auto window = std::make_shared<gua::GlfwWindow>();
     gua::WindowDatabase::instance()->add("main_window", window);
 
@@ -174,6 +209,7 @@ int main(int argc, char** argv)
         std::bind(key_press, std::ref(*(camera->get_pipeline_description())), std::ref(graph), std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4));
 
 
+    // the renderer triggers the execution of the pipeline according t our description
     gua::Renderer renderer;
 
     // application loop
@@ -190,15 +226,16 @@ int main(int argc, char** argv)
         std::cout << std::endl;
 
         // apply trackball matrix to object
+        // we update the model transformatin according to the internal state of the trackball.
         gua::math::mat4 transform_node_model_matrix = scm::math::make_translation(trackball.shiftx(), trackball.shifty(), trackball.distance()) * gua::math::mat4(trackball.rotation());
 
-
+        // set transform completely overrides the old transformation of this node
         transform_node->set_transform(transform_node_model_matrix);
+        //translate, rotate, and scale apply a corresponding matrix to the current matrix stored in the node
         transform_node->translate(0.0f, 0.0f, -5.0f);
 
 
-
-
+        //we ask the renderer for the currently averaged applicatin fps and the window for the rendering fps
         float application_fps = renderer.get_application_fps();
         float rendering_fps   = window->get_rendering_fps();
 
