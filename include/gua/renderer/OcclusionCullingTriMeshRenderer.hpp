2f3a47875e0b203ba7ec62fe1d4def0cd8407de3
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2019-11-19 10:59:09 +0100

    refactor rendering code in occlusion culling trimesh renderer

--- include/gua/renderer/OcclusionCullingTriMeshRenderer.hpp
+++ include/gua/renderer/OcclusionCullingTriMeshRenderer.hpp
@@ -71,7 +71,12 @@ class GUA_DLL OcclusionCullingTriMeshRenderer
     void render_without_oc(Pipeline& pipe, PipelinePassDescription const& desc);
     void render_hierarchical_stop_and_wait_oc(Pipeline& pipe, PipelinePassDescription const& desc);
 
+    void switch_state_for_depth_complexity_vis(RenderContext const& ctx, std::shared_ptr<ShaderProgram>& active_shader);
+    void switch_state_based_on_node_material(RenderContext const& ctx, node::TriMeshNode* tri_mesh_node, std::shared_ptr<ShaderProgram>& current_shader, 
+                                             MaterialShader* current_material, RenderTarget const& target, bool shadow_mode, std::size_t cam_uuid);
 
+    void upload_uniforms_for_node(RenderContext const& ctx, node::TriMeshNode* tri_mesh_node, std::shared_ptr<ShaderProgram>& current_shader, 
+                                  Pipeline& pipe, scm::gl::rasterizer_state_ptr& current_rasterizer_state);
 
   private:
 
