65d10c6a82659f1f0124379d2611c59574e23e43
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2019-11-25 09:02:05 +0100

    add coarse depth sorting dummy

--- include/gua/renderer/PipelinePass.hpp
+++ include/gua/renderer/PipelinePass.hpp
@@ -128,12 +128,16 @@ class GUA_DLL PipelinePassDescription
     uint64_t get_occlusion_culling_fragment_threshold() const;
     void set_occlusion_culling_fragment_threshold(uint64_t fragment_threshold);
 
+    void set_enable_coarse_depth_sorting(bool enable);
+    bool get_enable_coarse_depth_sorting() const;
+
     void enable(bool enable);
     bool is_enabled() const;
 
   private:
     void* user_data_ = nullptr;
 
+    bool enable_coarse_depth_sorting_ = false;
 
     // describes the occlusion culling strategy
     OcclusionCullingStrategy occlusion_culling_strategy_ = OcclusionCullingStrategy::No_Culling;
