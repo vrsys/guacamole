c3114475b8835175b331d5b77001c1eb535d7f15
Theresa Tobollik <voxu7843@zagreus.medien.uni-weimar.de>
2019-11-26 19:07:54 +0100

    create new scene for testing oc-culling

--- examples/occlusion_culling/scene_utils.hpp
+++ examples/occlusion_culling/scene_utils.hpp
@@ -9,6 +9,8 @@ void print_draw_times(gua::Renderer const& renderer, std::shared_ptr<gua::GlfwWi
 void print_graph(std::shared_ptr<gua::node::Node> const& scene_root_node, int depth = 0);
 
 void place_objects_randomly(std::string const& model_path, int32_t num_models_to_place, float random_pos_cube_dimensions, std::shared_ptr<gua::node::Node> scene_root_node);
+void create_occlusion_scene(std::string const& model_path_plane, std::string const& model_path_objects, std::shared_ptr<gua::node::Node> scene_root_node);
+
 
 void show_scene_bounding_boxes(std::shared_ptr<gua::node::Node> const& scene_root_node, bool enable, int bb_vis_level = -1, int current_node_level = 0);
 
