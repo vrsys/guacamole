0b8748dcc78e1c8b63af477f43f62746b023e45d
Theresa Tobollik <voxu7843@orestes.medien.uni-weimar.de>
2019-12-02 14:36:41 +0100

    small change to num_grouped_faces in TriMeshNode

--- src/gua/node/TriMeshNode.cpp
+++ src/gua/node/TriMeshNode.cpp
@@ -56,11 +56,13 @@ std::shared_ptr<Material> const& TriMeshNode::get_material() const { return mate
 
 ////////////////////////////////////////////////////////////////////////////////
 std::size_t TriMeshNode::num_grouped_faces() const  {
+
+  std::size_t node_count_of_children = Node::num_grouped_faces();
   if(geometry_) {
-    return geometry_->num_faces();
-  } else {
-    return 0;
+    node_count_of_children += geometry_->num_faces();
   }
+
+  return node_count_of_children;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
