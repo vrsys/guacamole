2958ad51ed24fe25771df452ace54e99963c74d2
Lucky Chandrautama <gejo4207@minos.medien.uni-weimar.de>
2019-12-16 20:15:57 +0100

    readding median split

--- src/gua/node/OcclusionCullingGroupNode.cpp
+++ src/gua/node/OcclusionCullingGroupNode.cpp
@@ -42,6 +42,120 @@ OcclusionCullingGroupNode::OcclusionCullingGroupNode(std::string const& name, ma
 std::shared_ptr<Node> OcclusionCullingGroupNode::copy() const { return std::make_shared<OcclusionCullingGroupNode>(*this); }
 
 
+
+void OcclusionCullingGroupNode::median_regroup_children(){
+    std::queue<gua::node::Node*> splitting_queue;
+
+
+    //if we have less than 3 children, then the grouping is as good as it gets
+    std::vector<std::shared_ptr<Node>> children = get_children();
+
+    if(children.size() > 2) {
+        splitting_queue.push(this);
+    }
+
+    while(!splitting_queue.empty()) {
+
+        std::array<std::vector<std::shared_ptr<gua::node::Node> >, 3> children_sorted_by_xyz;
+        auto current_node_to_split = splitting_queue.front();
+        splitting_queue.pop();
+
+        current_node_to_split->update_cache();
+
+        int best_splitting_axis = -1;
+        double best_splitting_cost = std::numeric_limits<double>::max(); // get the maximal value for our type
+
+
+        for(unsigned int dim_idx = 0; dim_idx < 3; ++dim_idx) {
+            children_sorted_by_xyz[dim_idx] = current_node_to_split->get_children();
+        }
+
+        for(unsigned int dim_idx = 0; dim_idx < 3; ++dim_idx) {
+
+            sorting_based_on_axis(children_sorted_by_xyz[dim_idx], dim_idx);
+            unsigned int const median = children_sorted_by_xyz[dim_idx].size()/2;
+            split_children(current_node_to_split, children_sorted_by_xyz[dim_idx],median, 1);
+            
+            double cost_for_current_axis = calculate_median_cost(current_node_to_split);
+
+            if(cost_for_current_axis < best_splitting_cost) {
+                best_splitting_cost = cost_for_current_axis;
+                best_splitting_axis = dim_idx;
+            }
+
+            cleanup_intermediate_nodes(current_node_to_split);
+        }
+
+        unsigned int const best_median = children_sorted_by_xyz[best_splitting_axis].size()/2;
+
+        split_children(current_node_to_split, children_sorted_by_xyz[best_splitting_axis],best_median, 1);
+        auto children = current_node_to_split->get_children();
+
+        for(unsigned int child_idx = 0; child_idx < 2; ++child_idx) {
+            auto& current_child = children[child_idx];
+
+            auto& current_grandchildren = current_child->get_children();
+
+            if(current_grandchildren.size() > 2) {
+                gua::node::Node* child_ptr = children[child_idx].get();
+                splitting_queue.push(child_ptr);
+
+            }
+
+        }
+    }
+/*
+    while(!splitting_queue.empty() ) {
+        //get next node to split
+        auto current_node_to_split = splitting_queue.front();
+        splitting_queue.pop();
+
+
+        current_node_to_split->update_cache();
+
+        int best_splitting_axis = -1;
+        double best_splitting_cost = std::numeric_limits<double>::max(); // get the maximal value for our type
+
+
+        for(unsigned int dim_idx = 0; dim_idx < 3; ++dim_idx) {
+            children_sorted_by_xyz[dim_idx] = current_node_to_split->get_children();
+        }
+
+        for(unsigned int dim_idx = 0; dim_idx < 3; ++dim_idx) {
+
+            sorting_based_on_axis(children_sorted_by_xyz[dim_idx], dim_idx);
+
+            split_children(current_node_to_split, children_sorted_by_xyz[dim_idx],children_sorted_by_xyz[dim_idx].size()/2, 1);
+            
+            double cost_for_current_axis = calculate_cost(current_node_to_split);
+
+            if(cost_for_current_axis < best_splitting_cost) {
+                best_splitting_cost = cost_for_current_axis;
+                best_splitting_axis = dim_idx;
+            }
+
+            cleanup_intermediate_nodes(current_node_to_split);
+        }
+
+
+        split_children(current_node_to_split, children_sorted_by_xyz[best_splitting_axis],children_sorted_by_xyz[best_splitting_axis].size()/2, 1);
+
+        auto children = current_node_to_split->get_children();
+
+        for(unsigned int child_idx = 0; child_idx < 2; ++child_idx) {
+            auto& current_child = children[child_idx];
+
+            if(current_child->get_children().size() > 2) {
+                gua::node::Node* child_ptr = children[child_idx].get();
+                splitting_queue.push(child_ptr);
+            }
+
+        }
+    }
+*/
+
+}
+
 void OcclusionCullingGroupNode::regroup_children(){
 
 
@@ -234,6 +348,25 @@ double OcclusionCullingGroupNode::calculate_cost(gua::node::Node* node){
 
 }
 
+double OcclusionCullingGroupNode::calculate_median_cost(gua::node::Node* node){
+    node->update_cache();
+    auto parent_surface_area = node->get_bounding_box().surface_area();
 
+#ifdef MAKE_OCCLUSION_CULLING_APP_VERBOSE
+    std::cout << "Surface area Parent: " << parent_surface_area << std::endl;
+#endif //MAKE_OCCLUSION_CULLING_APP_VERBOSE
+
+    auto children = node->get_children();
+
+    float summed_weighted_surface_area = 0.0f;
+
+    for(auto& child : children) {
+        child->update_cache();
+        summed_weighted_surface_area += child->get_bounding_box().surface_area() * child->num_grouped_faces();
+    }
+
+    return (summed_weighted_surface_area) / parent_surface_area;
+
+}
 } // namespace node
 } // namespace gua
