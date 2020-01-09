2f72b303169bd843171beb67ced51df5334764c3
Nyran <adrian.kreskowski@uni-weimar.de>
2020-01-10 00:38:40 +0100

    only write normals to gbuffer if emissivity of front most pixel is < 1

--- resources/shaders/common/gua_write_gbuffer.glsl
+++ resources/shaders/common/gua_write_gbuffer.glsl
@@ -1,7 +1,10 @@
 gua_out_color     = gua_color;
 
 gua_out_pbr       = vec3(gua_emissivity, gua_roughness, gua_metalness);
-gua_out_normal    = gua_normal*0.5+0.5;
+
+if(gua_emissivity < 1.0) { 
+	gua_out_normal = gua_normal*0.5+0.5;
+}
 
 gua_out_flags = gua_flags_passthrough ? 1u : 0;
 
