7575482718c86092032cc5ee21d32b2582cabf8e
Nyran <adrian.kreskowski@uni-weimar.de>
2020-01-10 00:39:30 +0100

    revert normal optimization

--- resources/shaders/common/gua_write_gbuffer.glsl
+++ resources/shaders/common/gua_write_gbuffer.glsl
@@ -2,9 +2,9 @@ gua_out_color     = gua_color;
 
 gua_out_pbr       = vec3(gua_emissivity, gua_roughness, gua_metalness);
 
-if(gua_emissivity < 1.0) { 
+//if(gua_emissivity < 1.0) { 
 	gua_out_normal = gua_normal*0.5+0.5;
-}
+//}
 
 gua_out_flags = gua_flags_passthrough ? 1u : 0;
 
