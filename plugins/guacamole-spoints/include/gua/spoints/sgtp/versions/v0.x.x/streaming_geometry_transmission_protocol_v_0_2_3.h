25e882e2b8076cf9490920c6bd2d8f981b832cf5
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2020-02-26 12:27:24 +0100

    update decompression of jpeg streams in parallell

--- plugins/guacamole-spoints/include/gua/spoints/sgtp/versions/v0.x.x/streaming_geometry_transmission_protocol_v_0_2_3.h
+++ plugins/guacamole-spoints/include/gua/spoints/sgtp/versions/v0.x.x/streaming_geometry_transmission_protocol_v_0_2_3.h
@@ -11,13 +11,13 @@ using chrono_timestamp = std::chrono::time_point<std::chrono::system_clock>;
 namespace SGTP {
   uint8_t     const _MAX_NUM_SENSORS = 16;
   std::size_t const _TEXTURE_DIMENSION_X = 2*2560;
-  std::size_t const _TEXTURE_DIMENSION_Y = 2*1280;
+  std::size_t const _TEXTURE_DIMENSION_Y = 2*1440;
 
   namespace v_0_2_3{
 
-  	uint32_t const _MAJOR_VERSION = 0U;
-  	uint32_t const _MINOR_VERSION = 2U;
-  	uint32_t const _MICRO_VERSION = 3U;
+    uint32_t const _MAJOR_VERSION = 0U;
+    uint32_t const _MINOR_VERSION = 2U;
+    uint32_t const _MICRO_VERSION = 3U;
 
   struct _uint16_pos2d_t {
     uint16_t u = std::numeric_limits<uint16_t>::max();
@@ -33,40 +33,48 @@ namespace SGTP {
     _uint16_pos2d_t& operator[](int32_t const idx) { return ((idx == 0) ? min : max ); }
   };
 
-	struct _header_data_t {
+  struct _header_data_t {
     bool        is_calibration_data             = false;
 
-	  // data filled in case is_calibration_data = false
+    // data filled in case is_calibration_data = false
     bool        is_data_compressed        = true;
     bool        is_fully_encoded_vertex_data = false;
     float                    global_bb_min[3]                = {0, 0, 0}; 
-	  float                    global_bb_max[3]                = {0, 0, 0};  
-	  float                    timestamp                       = -1.0f;
-	  float                    geometry_creation_time_in_ms    = -1.0f;
-	  uint32_t                 num_textured_triangles       = 0;
+    float                    global_bb_max[3]                = {0, 0, 0};  
+    float                    timestamp                       = -1.0f;
+    float                    geometry_creation_time_in_ms    = -1.0f;
+
+    float                    box_division_time_in_ms         = -1.0f;
+    float                    texture_processing_time_in_ms   = -1.0f;
+    float                    frustum_and_occlusion_culling_time_in_ms = -1.0f;
+    float                    integration_time_in_ms          = -1.0f;
+    float                    marching_cubes_time_in_ms       = -1.0f;
+    float                    atlas_generation_time_in_ms     = -1.0f;
+
+    uint32_t                 num_textured_triangles       = 0;
     uint32_t                 geometry_payload_size        = 0;         
-	  uint32_t                 texture_payload_size         = 0;         
-	  uint32_t                 num_best_triangles_per_sensor[_MAX_NUM_SENSORS]   = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    uint32_t                 texture_payload_size         = 0;         
+    uint32_t                 num_best_triangles_per_sensor[_MAX_NUM_SENSORS]   = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     _texture_bounding_box_t  tex_bounding_box[_MAX_NUM_SENSORS];
     uint32_t                 bounding_box_pixel_coverage[_MAX_NUM_SENSORS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     uint32_t                 jpeg_bytes_per_sensor[_MAX_NUM_SENSORS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	  float                    lod_scaling = 1.0;
+    float                    lod_scaling = 1.0;
     float                    texture_lod_scaling = 1.0;
     int32_t                  package_reply_id = -1;
     int64_t                  passed_microseconds_since_request;
 
 
-	  // data filled in case (is_calibration_data == true)
-	  uint32_t    inv_xyz_volume_res[3]           = {0, 0, 0};
-	  uint32_t    uv_volume_res[3]                = {0, 0, 0};
-	  uint32_t    num_sensors		      = 0;
-	  float       inv_vol_to_world_mat[16]        = {1.0, 0.0, 0.0, 0.0,
-	  						                                   0.0, 1.0, 0.0, 0.0,
-	  						                                   0.0, 0.0, 1.0, 0.0,
-	  						                                   0.0, 0.0, 0.0, 1.0};
+    // data filled in case (is_calibration_data == true)
+    uint32_t    inv_xyz_volume_res[3]           = {0, 0, 0};
+    uint32_t    uv_volume_res[3]                = {0, 0, 0};
+    uint32_t    num_sensors         = 0;
+    float       inv_vol_to_world_mat[16]        = {1.0, 0.0, 0.0, 0.0,
+                                                   0.0, 1.0, 0.0, 0.0,
+                                                   0.0, 0.0, 1.0, 0.0,
+                                                   0.0, 0.0, 0.0, 1.0};
 
-	  // contains total payload of either avatar or calibration data
-	  uint32_t    total_payload                = 0;
+    // contains total payload of either avatar or calibration data
+    uint32_t    total_payload                = 0;
 
     void fill_texture_byte_offsets_to_bounding_boxes() {
 
@@ -80,7 +88,7 @@ namespace SGTP {
       }
 
     }
-	};
+  };
 
     struct _vertex_data_t {
       // offset:
